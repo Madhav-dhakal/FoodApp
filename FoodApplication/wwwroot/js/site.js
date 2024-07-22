57f5737610c9c8fad9a2264031b9e5a5c66cff00
Madhav-dhakal <madhavdhakal19251@gmail.com>
2024-07-22 15:43:23 +0545

    api datafetch and recipecard

--- FoodApplication/wwwroot/js/site.js
+++ FoodApplication/wwwroot/js/site.js
@@ -1,4 +1,25 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification.
 // Write your JavaScript code.
+
+let apiURL = "https://forkify-api.herokuapp.com/api/v2/recipes";
+let apiKey = "17d3a93d-eb88-420b-82a6-5862b2bbf364";
+async function GetRecipes(recipeName,id,isAllShow) {
+    let resp = await fetch(`${apiURL}?search=${recipeName}&key=${apiKey}`);
+    let result = await resp.json();
+    console.log(result);
+    let Recipes = isAllShow ? result.data.recipes : result.data.recipes.slice(0, 6);
+    showRecipes(Recipes, id);
+}
+function showRecipes(recipes, id) {
+    $.ajax({
+        contentType: "application/json; charset=utf-8",
+        dataType: "html",
+        type: "POST",
+        url: "/Recipe/GetRecipeCard",
+        data: JSON.stringify(recipes),
+        success: function (htmlResult) {
+            $('#' + id).html(htmlResult);
+        },
+
+    })
+}
